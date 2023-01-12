b9eddfeb7ab9894a1a5d14db5d0ea0a1ba38caed
Richard <richard.frei@hotmail.com>
2023-01-12 10:49:46 -0300

    Notas de aula e código sobre tipos e Variáveis. Inclusão do módulo operadores.

---
+++ aprendendo_sintaxe_java/2-tipos_e_variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        byte idade = 25;
+        short ano = 2023;
+        // Para declarar uma variável do tipo float,
+        // devemos colocar um "F" depois do número
+        float altura = 1.75F;
+        boolean estudante = true;
+        long cpf = 1234567890;
+        double salario = 1234.56;
+
+        final double VALOR_DE_PI = 3.14159265358979323846;
+
+
+    }
+}
---
+++ aprendendo_sintaxe_java/3-operadores/operadores.md
@@ -0,0 +1,1 @@
+# <p style="text-align: center">Operadores</p>
