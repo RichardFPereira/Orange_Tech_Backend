cc19ca5cecb97175f1c552b9cb169a266c9fec86
Richard <richard.frei@hotmail.com>
2023-01-26 20:43:31 -0300

    Arquivos de Operadores Lógicos criados para anotação de aula e prática de exercícios

--- logica-condicional-e-controle-de-fluxos-em-java/src/operadoreslogicos/OperadoresLogicos.java
+++ logica-condicional-e-controle-de-fluxos-em-java/src/operadoreslogicos/OperadoresLogicos.java
@@ -5,7 +5,24 @@ public class OperadoresLogicos {
         boolean b2 = false;
         boolean b3 = true;
         boolean b4 = false;
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 10.5f;
+        float f2 = 10.2f;
 
         System.out.println("b1 && b2: " + (b1 && b2));
+        System.out.println("b1 && b3: " + (b1 && b3));
+
+        System.out.println("b2 || b3: " + (b2 || b3));
+        System.out.println("b4 || b1: " + (b4 || b1));
+
+        System.out.println("b1 ^ b3: " + (b1 ^ b3));
+        System.out.println("b4 ^ b1: " + (b4 ^ b1));
+
+        System.out.println("!b1: " + (!b1));
+        System.out.println("!b2: " + (!b2));
+
+        System.out.println("(i1 > i2) || (f2 < f1): " + ((i1 > i2) || (f2 < f1)));
+        System.out.println("((i1 + i2) < (f2 - f1) && true): " + ((i1 + i2) < (f2 - f1) && true));
     }
 }
